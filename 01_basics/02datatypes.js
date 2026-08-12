a9882fc9226602b6e174bf77224cc2e963e6b0f2
piyush3370 <piyushsahab7403@gmail.com>
2026-08-12 17:57:55 +0000

    datatypes

--- 01_basics/02datatypes.js
+++ 01_basics/02datatypes.js
@@ -1,3 +1,27 @@
 "use strict"; // treat all JS code as newer version 
 
-alert (3+3)
+// alert (3+3) // we are using nodejs , not browser 
+
+console.log(3+3); 
+console.log("piyush")
+
+let name = "piyush"
+let age = 18 
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+//bigint
+// string => ""
+//boolean => true & false 
+// null => standalone value 
+// undefined => 
+// symbol => unique
+
+
+//oject
+
+console.log(typeof "piyush");
+console.log(typeof null);
+console.log(typeof undefined);
+
